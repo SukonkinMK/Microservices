abc52c4d0fe5c0cb01918add69a810cd4cc094fe
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2024-01-25 00:32:53 +0300

    добавлен метод получения платформы по id

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -18,13 +18,23 @@ namespace PlatformService.Controllers
             _mapper = mapper;
         }
 
-        [HttpGet("get_platforms")]
+        [HttpGet("get_all_platforms")]
         public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
         {
             var platformItems = _repository.GetAllPlatforms();
             //2 варианта с LINQ и без
             return Ok(platformItems.Select(x => _mapper.Map<PlatformReadDto>(x)));
             //return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+        }
+        [HttpGet("get_platform_by_id")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+            if(platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+            return NotFound();
         }
     }
 }
